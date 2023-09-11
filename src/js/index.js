4919ab3c90ef4bba2f64f142329920f6634c7069
johncleece <47151315+johncleece@users.noreply.github.com>
2023-09-11 19:19:14 +0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -13,15 +13,3 @@
   }
 })();
 
-// sale function
-function displayImage(imageSrc) {
-  var imageContainer = document.getElementById('imagecontainer');
-  // Create a new image element
-  var img = document.createElement('img');
-  img.src = imageSrc;
-  img.alt = 'Selected Image';
-  // Clear the contents of the image container
-  imageContainer.innerHTML = '';
-  // Append the selected image to the container
-  imageContainer.appendChild(img);
-}
