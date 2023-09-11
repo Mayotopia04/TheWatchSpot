a1190f582f86dda83454423c18650a3044e39cea
Joseph <josephmacayangarcia@gmail.com>
2023-09-11 18:38:46 +0800

    sale update
    update css

--- src/js/sale.js
+++ src/js/sale.js
@@ -1,14 +1,11 @@
 function displayImage(imageSrc) {
-  var imageContainer = document.getElementById("imagecontainer");
+  var imageContainer = document.getElementById('imagecontainer');
   // Create a new image element
-  var img = document.createElement("img");
-  img.classList = "sale-watch-preview";
+  var img = document.createElement('img');
   img.src = imageSrc;
-  img.alt = "Selected Image";
-  img.height = 350;
-  img.width = 235;
+  img.alt = 'Selected Image';
   // Clear the contents of the image container
-  imageContainer.innerHTML = "";
+  imageContainer.innerHTML = '';
   // Append the selected image to the container
   imageContainer.appendChild(img);
 }
