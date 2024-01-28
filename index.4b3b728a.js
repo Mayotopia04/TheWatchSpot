8798a60e91b3e3f60d853765a52c2b13ecb79a84
Mayotopia04 <134679840+Mayotopia04@users.noreply.github.com>
2024-01-28 11:16:51 +0000

    Deploying to gh-pages from @ Mayotopia04/TheWatchSpot@0abe0ffa98a7cb1eebabd303af8d8fbd8b9de177 🚀

---
+++ index.4b3b728a.js
@@ -0,0 +1,2 @@
+function displayImage(e){document.getElementById("imagecontainer");var a=document.querySelectorAll(".sale-watch-preview");a.forEach((function(e){e.classList.remove("activeimage")})),a[e].classList.add("activeimage")}
+//# sourceMappingURL=index.4b3b728a.js.map
--- index.7667a88a.js
+++
@@ -1,2 +0,0 @@
-function displayImage(e){var n=document.getElementById("imagecontainer"),t=document.createElement("img");t.src=e,t.alt="Selected Image",n.innerHTML="",n.appendChild(t)}
-//# sourceMappingURL=index.7667a88a.js.map
